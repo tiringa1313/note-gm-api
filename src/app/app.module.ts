570ce49369e039e40fcf01246aaa2d1f6d62bce4
tiringa1313 <155318981+tiringa1313@users.noreply.github.com>
2025-03-03 11:25:42 -0300

    foi feito alteracao no env para ajustar ao banco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/auth/auth.module';
+
 
 @Module({
   imports: [
