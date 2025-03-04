05a140146a30c9dee4ffaca879b055935e6f8df9
tiringa1313 <155318981+tiringa1313@users.noreply.github.com>
2025-03-04 10:11:49 -0300

    sincronize ativado para criar as tabelas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { AuthModule } from '../auth/auth.module';
         url: configService.get<string>('DATABASE_URL') || 
              'postgresql://frota_db_gu2a_user:t2QPsGG2gzMnL99EYvTsLVqCFMg21CJR@dpg-cuv1udt6l47c73eemtt0-a.oregon-postgres.render.com:5432/frota_db_gu2a', // 🔥 Agora com a porta correta
         autoLoadEntities: true, 
-        synchronize: false, // ❌ Nunca use true em produção
+        synchronize: true, // ❌ Nunca use true em produção
       }),
     }),
     UsersModule,
